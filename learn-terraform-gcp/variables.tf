451ba75cdb7b50873d2acd56161211e4ca3c2218
rthomas <45794704+remithomasn7@users.noreply.github.com>
2024-04-25 15:01:45 +0200

    Create variables.tf

---
+++ learn-terraform-gcp/variables.tf
@@ -0,0 +1,20 @@
+variable "project" { }
+
+variable "credentials_file" { }
+
+variable "region" {
+    default = "europe-southwest1"
+}
+
+variable "zone" {
+    default = "europe-southwest1-a"
+}
+
+
+variable "region_2" {
+    default = "europe-west1"
+}
+
+variable "zone_2" {
+    default = "europe-west1-b"
+}
